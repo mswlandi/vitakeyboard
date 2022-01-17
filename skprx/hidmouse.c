6bf23077820d7be225e835d6cf1639133eeb2756
MSWLandi <fakepsnmarcos@gmail.com>
2022-01-16 21:10:10 -0300

    modify descriptors to emulate keyboard

--- skprx/hidmouse.c
+++ skprx/hidmouse.c
@@ -3,284 +3,19 @@
 #include <psp2kern/ctrl.h>
 #include <psp2kern/udcd.h>
 
+#include "usb_descriptors.h"
 #include "hidmouse.h"
 
 #define Kprintf(...) (void)0
 
-#define PSP_USB_MOUSE            "PSP_MOUSE"
-#define PSP_USB_MOUSE_PID        0x7d
+#define PSP_USB_MOUSE            "VITA_KEYBOARD"
+#define PSP_USB_MOUSE_PID        0x1338
 
-static char g_inputs[4] __attribute__ ((aligned(64))) = { 0x00, 0x00, 0x00, 0x00 };
+static char g_inputs[8] __attribute__ ((aligned(64))) = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 static struct SceUdcdDeviceRequest g_request;
 static struct SceUdcdDeviceRequest g_reportrequest;
 static SceUID g_thid = -1;
 static int g_run = 1;
-
-static
-unsigned char hid_report[] __attribute__ ((aligned(64))) = {
-  0x05, 0x01,  /* Usage Page (Generic Desktop) */
-  0x09, 0x02,  /* Usage (Mouse) */
-  0xA1, 0x01,  /* Collection (Application) */
-  0x09, 0x01,  /* Usage (Pointer) */
-  0xA1, 0x00,  /* Collection (Physical) */
-  0x05, 0x09,  /* Usage Page (Button) */
-  0x19, 0x01,  /* Usage Minimum (Button 1) */
-  0x29, 0x03,  /* Usage Maximum (Button 3) */
-  0x15, 0x00,  /* Logical Minimum (0) */
-  0x25, 0x01,  /* Logical Maximum (1) */
-  0x95, 0x03,  /* Report Count (3) */
-  0x75, 0x01,  /* Report Size (1) */
-  0x81, 0x02,  /* Input (Data,Var,Abs,NWrp,Lin,Pref,NNul,Bit) */
-  0x95, 0x01,  /* Report Count (1) */
-  0x75, 0x05,  /* Report Size (5) */
-  0x81, 0x01,  /* Input (Cnst,Ary,Abs) */
-  0x05, 0x01,  /* Usage Page (Generic Desktop) */
-  0x09, 0x30,  /* Usage (X) */
-  0x09, 0x31,  /* Usage (Y) */
-  0x09, 0x38,  /* Usage (Wheel) */
-  0x15, 0x81,  /* Logical Minimum (-127) */
-  0x25, 0x7F,  /* Logical Maximum (127) */
-  0x75, 0x08,  /* Report Size (8) */
-  0x95, 0x03,  /* Report Count (3) */
-  0x81, 0x06,  /* Input (Data,Var,Rel,NWrp,Lin,Pref,NNul,Bit) */
-  0xC0,        /* End Collection */
-  0xC0         /* End Collection */
-} ;
-
-
-/* HID descriptor */
-static
-unsigned char hiddesc[] = {
-  0x09, /* bLength */
-  0x21, /* bDescriptorType */
-  0x10, 0x01, /* bcdHID */
-  0x00, /* bCountryCode */
-  0x01, /* bNumDescriptors */
-  0x22, /* bDescriptorType */
-  sizeof (hid_report), 0x00 /* wDescriptorLength */
-};
-
-/* Endpoint blocks */
-static
-struct SceUdcdEndpoint endpoints[2] = {
-  { 0x00, 0, 0, 0 },
-  { 0x80, 1, 0, 0 }
-};
-
-/* Interfaces */
-static
-struct SceUdcdInterface interfaces[1] = {
-  { -1, 0, 1 }
-};
-
-/* String descriptor */
-static
-struct SceUdcdStringDescriptor descriptors[2] = {
-  {
-    16,
-    USB_DT_STRING,
-    { 'M', 'o', 'u', 's', 'e',  ' ', '1' }
-  },
-  {
-    0,
-    USB_DT_STRING
-  }
-};
-
-/* HI-Speed device descriptor */
-static
-struct SceUdcdDeviceDescriptor devdesc_hi =
-{
-  USB_DT_DEVICE_SIZE,
-  USB_DT_DEVICE,
-  0x200,         /* bcdUSB */
-  USB_CLASS_PER_INTERFACE,  /* bDeviceClass */
-  0,             /* bDeviceSubClass */
-  0,             /* bDeviceProtocol */
-  64,            /* bMaxPacketSize0 */
-  0,             /* idProduct */
-  0,             /* idVendor */
-  0x100,         /* bcdDevice */
-  0,             /* iManufacturer */
-  0,             /* iProduct */
-  0,             /* iSerialNumber */
-  1              /* bNumConfigurations */
-};
-
-/* Hi-Speed endpoint descriptors */
-static
-struct SceUdcdEndpointDescriptor endpdesc_hi[2] =
-{
-  {
-    USB_DT_ENDPOINT_SIZE ,
-    USB_DT_ENDPOINT,
-    0x81, /* bEndpointAddress */
-    0x03, /* bmAttributes */
-    0x04, /* wMaxPacketSize */
-    0x05  /* bInterval */
-  },
-  {
-    0,
-  }
-};
-
-/* Hi-Speed interface descriptor */
-static
-struct SceUdcdInterfaceDescriptor interdesc_hi[2] =
-{
-  {
-    USB_DT_INTERFACE_SIZE,
-    USB_DT_INTERFACE,
-    0,      /* bInterfaceNumber */
-    0,      /* bAlternateSetting */
-    1,      /* bNumEndpoints */
-    USB_CLASS_HID ,   /* bInterfaceClass */
-    0x01,   /* bInterfaceSubClass */
-    0x02,   /* bInterfaceProtocol */
-    1,      /* iInterface */
-    &endpdesc_hi[0], /* endpoints */
-    hiddesc,
-    sizeof (hiddesc)
-  },
-  {
-    0
-  }
-};
-
-/* Hi-Speed settings */
-static
-struct SceUdcdInterfaceSettings settings_hi[1] =
-{
-  {
-    &interdesc_hi[0],
-    0,
-    1
-  }
-};
-
-/* Hi-Speed configuration descriptor */
-static
-struct SceUdcdConfigDescriptor confdesc_hi =
-{
-  USB_DT_CONFIG_SIZE,
-  USB_DT_CONFIG,
-  (USB_DT_INTERFACE_SIZE + USB_DT_CONFIG_SIZE + USB_DT_ENDPOINT_SIZE + sizeof (hiddesc)), /* wTotalLength */
-  1,      /* bNumInterfaces */
-  1,      /* bConfigurationValue */
-  0,      /* iConfiguration */
-  0xC0,   /* bmAttributes */
-  0,      /* bMaxPower */
-  &settings_hi[0]
-};
-
-
-/* Hi-Speed configuration */
-static
-struct SceUdcdConfiguration config_hi =
-{
-  &confdesc_hi,
-  &settings_hi[0],
-  &interdesc_hi[0],
-  &endpdesc_hi[0]
-};
-
-/* Full-Speed device descriptor */
-static
-struct SceUdcdDeviceDescriptor devdesc_full =
-{
-  USB_DT_DEVICE_SIZE,
-  USB_DT_DEVICE,
-  0x200,         /* bcdUSB (should be 0x110 but the PSVita freezes otherwise) */
-  USB_CLASS_PER_INTERFACE,  /* bDeviceClass */
-  0,             /* bDeviceSubClass */
-  0,             /* bDeviceProtocol */
-  8,             /* bMaxPacketSize0 */
-  0,             /* idProduct */
-  0,             /* idVendor */
-  0x200,         /* bcdDevice */
-  0,             /* iManufacturer */
-  0,             /* iProduct */
-  0,             /* iSerialNumber */
-  1              /* bNumConfigurations */
-};
-
-/* Full-Speed endpoint descriptors */
-static
-struct SceUdcdEndpointDescriptor endpdesc_full[2] =
-{
-  {
-    USB_DT_ENDPOINT_SIZE ,
-    USB_DT_ENDPOINT,
-    0x81, /* bEndpointAddress */
-    0x03, /* bmAttributes */
-    0x04, /* wMaxPacketSize */
-    0x05  /* bInterval */
-  },
-  {
-    0,
-  }
-};
-
-
-/* Full-Speed interface descriptor */
-static
-struct SceUdcdInterfaceDescriptor interdesc_full[2] =
-{
-  {
-    USB_DT_INTERFACE_SIZE,
-    USB_DT_INTERFACE,
-    0,      /* bInterfaceNumber */
-    0,      /* bAlternateSetting */
-    1,      /* bNumEndpoints */
-    USB_CLASS_HID,   /* bInterfaceClass */
-    0x01,   /* bInterfaceSubClass */
-    0x02,   /* bInterfaceProtocol */
-    1,      /* iInterface */
-    &endpdesc_full[0], /* endpoints */
-    hiddesc,
-    sizeof (hiddesc)
-  },
-  {
-    0
-  }
-};
-
-
-/* Full-Speed settings */
-static
-struct SceUdcdInterfaceSettings settings_full[1] =
-{
-  {
-    &interdesc_full[0],
-    0,
-    1
-  }
-};
-
-/* Full-Speed configuration descriptor */
-static
-struct SceUdcdConfigDescriptor confdesc_full =
-{
-  USB_DT_CONFIG_SIZE,
-  USB_DT_CONFIG,
-  (USB_DT_INTERFACE_SIZE + USB_DT_CONFIG_SIZE + USB_DT_ENDPOINT_SIZE + sizeof (hiddesc)), /* wTotalLength */
-  1,      /* bNumInterfaces */
-  1,      /* bConfigurationValue */
-  0,      /* iConfiguration */
-  0xC0,   /* bmAttributes */
-  0,      /* bMaxPower */
-  &settings_full[0]
-};
-
-/* Full-Speed configuration */
-static
-struct SceUdcdConfiguration config_full =
-{
-  &confdesc_full,
-  &settings_full[0],
-  &interdesc_full[0],
-  &endpdesc_full[0]
-};
 
 
 /* Forward define driver functions */
@@ -424,37 +159,35 @@ static
 int update_mouse (SceSize args, void *argp)
 {
   SceCtrlData pad;
-  unsigned char x0 = 0, y0 = 0;
-  int first = 1;
+  int pressed = 0;
+  int changed = 0;
 
   //ksceCtrlSetSamplingCycle (0);
   ksceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
   while (g_run) {
-    if (ksceCtrlReadBufferPositive (0, &pad, 1) >= 0) {
-      g_inputs[1] = (pad.lx >> 3) - x0;
-      g_inputs[2] = (pad.ly >> 3) - y0;
-      g_inputs[0] = g_inputs[3] = 0;
-      if (pad.buttons & SCE_CTRL_UP)
-        g_inputs[3] = +1;
-      if (pad.buttons & SCE_CTRL_DOWN)
-        g_inputs[3] = -1;
-      if (pad.buttons & SCE_CTRL_CROSS)
-        g_inputs[0] |= 1;
-      if (pad.buttons & SCE_CTRL_CIRCLE)
-        g_inputs[0] |= 2;
-      if (pad.buttons & SCE_CTRL_SQUARE)
-        g_inputs[0] |= 4;
-      ksceKernelCpuDcacheWritebackRange (g_inputs, sizeof (g_inputs));
 
+    ksceCtrlPeekBufferPositive(0, &pad, 1);
+
+    if (pad.buttons & SCE_CTRL_CROSS && !pressed)
+    {
+      g_inputs[3] = 0x10;
+      pressed = 1;
+      changed = 1;
     }
-    if (first) {
-      x0 = g_inputs[1];
-      y0 = g_inputs[2];
+    else if (!(pad.buttons & SCE_CTRL_CROSS) && pressed)
+    {
+      g_inputs[3] = 0x00;
+      pressed = 0;
+      changed = 1;
     }
-    if (ksceUdcdGetDeviceState () & SCE_UDCD_STATUS_CONNECTION_ESTABLISHED)
+
+    ksceKernelCpuDcacheAndL2WritebackRange(g_inputs, sizeof(g_inputs));
+
+    if (ksceUdcdGetDeviceState () & SCE_UDCD_STATUS_CONNECTION_ESTABLISHED && changed)
       send_inputs ();
-    ksceKernelDelayThread (20000);
-    first = 0;
+    ksceKernelDelayThread (10000);
+
+    changed = 0;
   }
   return 0;
 }
---
+++ skprx/usb_descriptors.h
@@ -0,0 +1,278 @@
+// The descriptors are xerpi/hidmouse's descriptors modified according to
+// https://www.usb.org/sites/default/files/documents/hid1_11.pdf
+// (page 76 - Appendix E: Example USB Descriptors for HID Class Devices)
+
+static
+unsigned char hid_report[] __attribute__((aligned(64))) = {
+  0x05, 0x01, // Usage Page(Generic Desktop),
+  0x09, 0x06, // Usage(Keyboard),
+  0xA1, 0x01, // Collection(Application),
+
+  0x05, 0x07, //  Usage Page(Key Codes);
+  0x19, 0xE0, //  Usage Minimum(224), [left control]
+  0x29, 0xE7, //  Usage Maximum(231), [right GUI]
+  0x15, 0x00, //  Logical Minimum(0),
+  0x25, 0x01, //  Logical Maximum(1),
+  0x75, 0x01, //  Report Size(1),
+  0x95, 0x08, //  Report Count(8),
+  0x81, 0x02, //  Input(Data, Variable, Absolute), [Modifier byte]
+
+  0x95, 0x01, //  Report Count(1),
+  0x75, 0x08, //  Report Size(8),
+  0x81, 0x01, //  Input(Constant), [Reserved byte]
+
+  0x95, 0x05, //  Report Count(5),
+  0x75, 0x01, //  Report Size(1),
+  0x05, 0x08, //  Usage Page (Page# for LEDs),
+  0x19, 0x01, //  Usage Minimum(1),
+  0x29, 0x05, //  Usage Maximum(5),
+  0x91, 0x02, //  Output(Data, Variable, Absolute), [LED report]
+
+  0x95, 0x01, //  Report Count(1),
+  0x75, 0x03, //  Report Size(3),
+  0x91, 0x01, //  Output(Constant), [LED report padding]
+
+  0x95, 0x06, //  Report Count(6),
+  0x75, 0x08, //  Report Size(8),
+  0x15, 0x00, //  Logical Minimum(0),
+  0x25, 0x65, //  Logical Maximum(101),
+  0x05, 0x07, //  Usage Page(Key Codes),
+  0x19, 0x00, //  Usage Minimum(0),
+  0x29, 0x65, //  Usage Maximum(101),
+  0x81, 0x00, //  Input(Data, Array), [Key arrays(6 bytes)]
+  0xC0        // End Collection
+};
+
+/* HID descriptor */
+static
+unsigned char hiddesc[] = {
+  0x09, /* bLength */
+  0x21, /* bDescriptorType */
+  0x11, 0x01, /* bcdHID */
+  0x00, /* bCountryCode */
+  0x01, /* bNumDescriptors */
+  0x22, /* bDescriptorType */
+  sizeof(hid_report), 0x00 /* wDescriptorLength */
+};
+
+/* Endpoint blocks */
+static
+struct SceUdcdEndpoint endpoints[2] = {
+  { 0x00, 0, 0, 0 },
+  { 0x80, 1, 0, 0 }
+};
+
+/* Interfaces */
+static
+struct SceUdcdInterface interfaces[1] = {
+  { -1, 0, 1 }
+};
+
+/* String descriptor */
+static
+struct SceUdcdStringDescriptor descriptors[2] = {
+  {
+    15,
+    USB_DT_STRING,
+    { 'v', 'i', 't', 'a', '-',  'k', 'e', 'y', 'b', 'o', 'a', 'r', 'd' }
+  },
+  {
+    0,
+    USB_DT_STRING
+  }
+};
+
+/* HI-Speed device descriptor */
+static
+struct SceUdcdDeviceDescriptor devdesc_hi =
+{
+  USB_DT_DEVICE_SIZE,
+  USB_DT_DEVICE,
+  0x200,         /* bcdUSB */
+  USB_CLASS_PER_INTERFACE,  /* bDeviceClass */
+  0,             /* bDeviceSubClass */
+  0,             /* bDeviceProtocol */
+  64,            /* bMaxPacketSize0 */
+  0,             /* idProduct */
+  0,             /* idVendor */
+  0x100,         /* bcdDevice */
+  0,             /* iManufacturer */
+  0,             /* iProduct */
+  0,             /* iSerialNumber */
+  1              /* bNumConfigurations */
+};
+
+/* Hi-Speed endpoint descriptors */
+static
+struct SceUdcdEndpointDescriptor endpdesc_hi[2] =
+{
+  {
+    USB_DT_ENDPOINT_SIZE ,
+    USB_DT_ENDPOINT,
+    0x81, /* bEndpointAddress */
+    0x03, /* bmAttributes */
+    0x40, /* wMaxPacketSize */
+    0x01  /* bInterval */
+  },
+  {
+    0,
+  }
+};
+
+/* Hi-Speed interface descriptor */
+static
+struct SceUdcdInterfaceDescriptor interdesc_hi[2] =
+{
+  {
+    USB_DT_INTERFACE_SIZE,
+    USB_DT_INTERFACE,
+    0,                /* bInterfaceNumber */
+    0,                /* bAlternateSetting */
+    1,                /* bNumEndpoints */
+    USB_CLASS_HID ,   /* bInterfaceClass */
+    0x00,             /* bInterfaceSubClass */  // boot interface subclass
+    0x00,             /* bInterfaceProtocol */  // keyboard
+    1,                /* iInterface */
+    &endpdesc_hi[0],  /* endpoints */
+    hiddesc,
+    sizeof(hiddesc)
+  },
+  {
+    0
+  }
+};
+
+/* Hi-Speed settings */
+static
+struct SceUdcdInterfaceSettings settings_hi[1] =
+{
+  {
+    &interdesc_hi[0],
+    0,
+    1
+  }
+};
+
+/* Hi-Speed configuration descriptor */
+static
+struct SceUdcdConfigDescriptor confdesc_hi =
+{
+  USB_DT_CONFIG_SIZE,
+  USB_DT_CONFIG,
+  (USB_DT_INTERFACE_SIZE + USB_DT_CONFIG_SIZE + USB_DT_ENDPOINT_SIZE + sizeof(hiddesc)), /* wTotalLength */
+  1,      /* bNumInterfaces */
+  1,      /* bConfigurationValue */
+  0,      /* iConfiguration */
+  0xC0,   /* bmAttributes */
+  0,      /* bMaxPower */
+  &settings_hi[0]
+};
+
+
+/* Hi-Speed configuration */
+static
+struct SceUdcdConfiguration config_hi =
+{
+  &confdesc_hi,
+  &settings_hi[0],
+  &interdesc_hi[0],
+  &endpdesc_hi[0]
+};
+
+/* Full-Speed device descriptor */
+static
+struct SceUdcdDeviceDescriptor devdesc_full =
+{
+  USB_DT_DEVICE_SIZE,
+  USB_DT_DEVICE,
+  0x200,                    /* bcdUSB (should be 0x110 but the PSVita freezes otherwise) */
+  USB_CLASS_PER_INTERFACE,  /* bDeviceClass */
+  0,                        /* bDeviceSubClass */
+  0,                        /* bDeviceProtocol */
+  8,                        /* bMaxPacketSize0 */
+  0,                        /* idProduct */
+  0,                        /* idVendor */
+  0x200,                    /* bcdDevice */
+  0,                        /* iManufacturer */
+  0,                        /* iProduct */
+  0,                        /* iSerialNumber */
+  1                         /* bNumConfigurations */
+};
+
+/* Full-Speed endpoint descriptors */
+static
+struct SceUdcdEndpointDescriptor endpdesc_full[2] =
+{
+  {
+    USB_DT_ENDPOINT_SIZE ,
+    USB_DT_ENDPOINT,
+    0x81, /* bEndpointAddress */
+    0x03, /* bmAttributes */
+    0x40, /* wMaxPacketSize */
+    0x01  /* bInterval */
+  },
+  {
+    0,
+  }
+};
+
+
+/* Full-Speed interface descriptor */
+static
+struct SceUdcdInterfaceDescriptor interdesc_full[2] =
+{
+  {
+    USB_DT_INTERFACE_SIZE,
+    USB_DT_INTERFACE,
+    0,                  /* bInterfaceNumber */
+    0,                  /* bAlternateSetting */
+    1,                  /* bNumEndpoints */
+    USB_CLASS_HID,      /* bInterfaceClass */
+    0x00,               /* bInterfaceSubClass */  // boot interface subclass
+    0x00,               /* bInterfaceProtocol */  // keyboard
+    1,                  /* iInterface */
+    &endpdesc_full[0],  /* endpoints */
+    hiddesc,
+    sizeof(hiddesc)
+  },
+  {
+    0
+  }
+};
+
+
+/* Full-Speed settings */
+static
+struct SceUdcdInterfaceSettings settings_full[1] =
+{
+  {
+    &interdesc_full[0],
+    0,
+    1
+  }
+};
+
+/* Full-Speed configuration descriptor */
+static
+struct SceUdcdConfigDescriptor confdesc_full =
+{
+  USB_DT_CONFIG_SIZE,
+  USB_DT_CONFIG,
+  (USB_DT_INTERFACE_SIZE + USB_DT_CONFIG_SIZE + USB_DT_ENDPOINT_SIZE + sizeof(hiddesc)), /* wTotalLength */
+  1,      /* bNumInterfaces */
+  1,      /* bConfigurationValue */
+  0,      /* iConfiguration */
+  0xC0,   /* bmAttributes */
+  0,      /* bMaxPower */
+  &settings_full[0]
+};
+
+/* Full-Speed configuration */
+static
+struct SceUdcdConfiguration config_full =
+{
+  &confdesc_full,
+  &settings_full[0],
+  &interdesc_full[0],
+  &endpdesc_full[0]
+};
