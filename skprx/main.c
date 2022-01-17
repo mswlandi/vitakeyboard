9ed819368aa1e3b4512c156c5b6ef84c362afad7
MSWLandi <fakepsnmarcos@gmail.com>
2022-01-16 22:29:53 -0300

    code cleanup

--- skprx/main.c
+++ skprx/main.c
@@ -5,16 +5,16 @@ void _start() __attribute__ ((weak, alias ("module_start")));
 
 int module_start (SceSize args, void *argp)
 {
-  int ret;
+    int ret;
 
-  ret = keyboard_start ();
-  if (ret < 0) return ret;
+    ret = keyboard_start ();
+    if (ret < 0) return ret;
 
-  return SCE_KERNEL_START_SUCCESS;
+    return SCE_KERNEL_START_SUCCESS;
 }
 
 int module_stop (SceSize args, void *argp)
 {
-  keyboard_stop ();
-  return SCE_KERNEL_STOP_SUCCESS;
+    keyboard_stop ();
+    return SCE_KERNEL_STOP_SUCCESS;
 }
--- vpk/main.c
+++ vpk/main.c
@@ -2,70 +2,120 @@
 #include <psp2/ctrl.h>
 #include <taihen.h>
 #include "debugScreen.h"
+#include <psp2/sysmodule.h>
+#include <psp2/libime.h>
 
 #define printf(...) psvDebugScreenPrintf(__VA_ARGS__)
 
 static SceUID modid;
+SceUInt32 libime_work[SCE_IME_WORK_BUFFER_SIZE / sizeof(SceInt32)];
+SceWChar16 libime_out[SCE_IME_MAX_PREEDIT_LENGTH + SCE_IME_MAX_TEXT_LENGTH + 1];
+char libime_initval[8] = { 1 };
+SceImeCaret caret_rev;
 
-static void wait_key_press();
-
-SceUID load_module (const char *path, int flags, int type)
-{
-  return taiLoadKernelModule (path, 0, NULL);
-}
+static void WaitKeyPress();
+SceUID LoadModule(const char *path, int flags, int type);
+void ImeEventHandler(void* arg, const SceImeEventData* e);
 
 int main (int argc, char **argv)
 {
-  int res, status;
+    int res;
 
-  psvDebugScreenInit();
+    psvDebugScreenInit();
 
-  printf ("Loading modules... ");
-  modid = load_module ("ux0:/tai/hidkeyboard.skprx", 0, 0);
-  if (modid < 0) {
-    printf ("failed with 0x%08X\n", modid);
-    printf("Place the skprx at ux0:/tai/hidkeyboard.skprx\n", modid);
-    wait_key_press();
-    return -1;
-  }
+    res = HidKeyboardInit();
+    if (res < 0) {
+        return -1;
+    }
 
-  printf ("OK\n");
-  printf ("Starting modules... ");
+    SceCtrlData pad;
+    printf("Press START to exit.\n");
+    while (1) {
+        sceCtrlPeekBufferPositive(0, &pad, 1);
+        if (pad.buttons & SCE_CTRL_START)
+            break;
+        sceKernelDelayThread(16 * 1000); // about 60 fps
+    }
 
-  res = taiStartKernelModule  (modid, 0, NULL, 0, NULL, &status);
-  if (res < 0) {
-    printf ("failed with 0x%08X\n", res);
-    wait_key_press();
-    return -1;
-  }
+    if (modid >= 0) {
+        taiStopUnloadKernelModule(modid, 0, NULL, 0, NULL, NULL);
+    }
 
-  if (status) {
-    printf ("failed with status = 0x%08X\n", status);
-    wait_key_press();
-    return -1;
-  }
-
-  wait_key_press();
-
-  if (modid >= 0) {
-    taiStopUnloadKernelModule(modid, 0, NULL, 0, NULL, NULL);
-  }
-
-  printf ("OK\n");
-  return 0;
+    printf ("OK\n");
+    return 0;
 }
 
-void wait_key_press()
+void WaitKeyPress()
 {
-  SceCtrlData pad;
+    SceCtrlData pad;
 
-  printf("Press START to exit.\n");
+    printf("Press START to exit.\n");
 
-  while (1) {
-    sceCtrlPeekBufferPositive(0, &pad, 1);
-    if (pad.buttons & SCE_CTRL_START)
-      break;
-    sceKernelDelayThread(100 * 1000);
-  }
+    while (1) {
+        sceCtrlPeekBufferPositive(0, &pad, 1);
+        if (pad.buttons & SCE_CTRL_START)
+        break;
+        sceKernelDelayThread(100 * 1000);
+    }
 }
 
+int HidKeyboardInit()
+{
+    int res, status;
+
+    printf("Loading hidkeyboard module... ");
+    modid = LoadModule("ux0:/tai/hidkeyboard.skprx", 0, 0);
+    if (modid < 0) {
+        printf("failed with 0x%08X\n", modid);
+        printf("Place the skprx at ux0:/tai/hidkeyboard.skprx\n", modid);
+        WaitKeyPress();
+        return -1;
+    }
+    printf("OK\n");
+
+    printf("Starting hidkeyboard module... ");
+    res = taiStartKernelModule(modid, 0, NULL, 0, NULL, &status);
+    if (res < 0) {
+        printf("failed with 0x%08X\n", res);
+        WaitKeyPress();
+        return -1;
+    }
+
+    if (status) {
+        printf("failed with status = 0x%08X\n", status);
+        WaitKeyPress();
+        return -1;
+    }
+}
+
+SceUID LoadModule(const char* path, int flags, int type)
+{
+    return taiLoadKernelModule(path, 0, NULL);
+}
+
+void ImeEventHandler(void* arg, const SceImeEventData* e)
+{
+    switch (e->id) {
+    case SCE_IME_EVENT_UPDATE_TEXT:
+        if (e->param.text.caretIndex == 0) {
+            // backspace
+            sceImeSetText((SceWChar16*)libime_initval, 4);
+        }
+        else {
+            // test for scancode from key
+            // if not space, utf16_toutf8 to send text
+            memset(&caret_rev, 0, sizeof(SceImeCaret));
+            memset(libime_out, 0, ((SCE_IME_MAX_PREEDIT_LENGTH + SCE_IME_MAX_TEXT_LENGTH + 1) * sizeof(SceWChar16)));
+            caret_rev.index = 1;
+            sceImeSetCaret(&caret_rev);
+            sceImeSetText((SceWChar16*)libime_initval, 4);
+        }
+        break;
+    case SCE_IME_EVENT_PRESS_ENTER:
+        // enter
+        break;
+    case SCE_IME_EVENT_PRESS_CLOSE:
+        sceImeClose();
+        break;
+    }
+}
