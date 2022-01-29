6b3b8471328d7dbf3f5612e17556ca5053bc052c
MSWLandi <fakepsnmarcos@gmail.com>
2022-01-29 09:24:26 -0300

    fix restarting and x sending backspace

--- vpk/main.c
+++ vpk/main.c
@@ -13,6 +13,7 @@ SceUInt32 libime_work[SCE_IME_WORK_BUFFER_SIZE / sizeof(SceInt32)];
 SceWChar16 libime_out[SCE_IME_MAX_PREEDIT_LENGTH + SCE_IME_MAX_TEXT_LENGTH + 1];
 char libime_initval[8] = { 1 };
 SceImeCaret caret_rev;
+int ime_just_closed = 0;
 
 static void WaitKeyPress();
 SceUID LoadModule(const char *path, int flags, int type);
@@ -30,6 +31,8 @@ int main (int argc, char **argv)
 
     printf("starting the module... ");
 
+    // stops before starting in case app was reset
+    hidkeyboard_user_stop();
     ret = hidkeyboard_user_start();
     if (ret >= 0) {
         printf("hidkeyboard started successfully!\n");
@@ -38,7 +41,7 @@ int main (int argc, char **argv)
         printf("hidkeyboard is already active!\n");
     }
     else if (ret < 0) {
-        printf("Error hidkeyboard_user_start(): 0x%08X\n", ret);
+        printf("Error on hidkeyboard_user_start(): 0x%08X\n", ret);
         WaitKeyPress();
         return -1;
     }
@@ -50,12 +53,19 @@ int main (int argc, char **argv)
         return -1;
     }
 
-    printf("Press START to exit.\n");
+    psvDebugScreenCoordX = 10 * SCREEN_GLYPH_W * PSV_DEBUG_SCALE;
+    psvDebugScreenCoordY = 25 * SCREEN_GLYPH_H * PSV_DEBUG_SCALE;
+    printf("Press X to reopen the virtual keyboard.\n");
     while (1) {
 
         sceCtrlPeekBufferPositive(0, &pad, 1);
-        if (pad.buttons & SCE_CTRL_START) {
-            break;
+        if (pad.buttons & SCE_CTRL_CROSS) {
+            ret = ImeInit();
+            if (ret < 0 && ret != SCE_IME_ERROR_ALREADY_OPENED) {
+                printf("Error on opening virtual keyboard: 0x%08X\n", ret);
+                WaitKeyPress();
+                break;
+            }
         }
         sceKernelDelayThread(10 * 1000); // about 100 fps
 
@@ -141,9 +151,15 @@ void ImeEventHandler(void* arg, const SceImeEventData* e)
     switch (e->id) {
     case SCE_IME_EVENT_UPDATE_TEXT:
         if (e->param.text.caretIndex == 0) {
-            // backspace
-            HidKeyBoardSendModifierAndKey(0x00, 0x2a);
-            sceImeSetText((SceWChar16*)libime_initval, 4);
+            // when you reopen the ime keyboard it updates text with an empty initial value
+            if (ime_just_closed) {
+                ime_just_closed = 0;
+            }
+            else {
+                // backspace
+                HidKeyBoardSendModifierAndKey(0x00, 0x2a);
+                sceImeSetText((SceWChar16*)libime_initval, 4);
+            }
         }
         else {
             // new character
@@ -162,6 +178,7 @@ void ImeEventHandler(void* arg, const SceImeEventData* e)
         HidKeyBoardSendModifierAndKey(0x00, 0x58);
         break;
     case SCE_IME_EVENT_PRESS_CLOSE:
+        ime_just_closed = 1;
         sceImeClose();
         break;
     }
